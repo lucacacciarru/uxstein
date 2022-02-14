58803afdda170836dbaca977193de2b77ea34a72
gabrieleAngius <gabrieleangius@protonmail.com>
2022-02-14 10:37:48 +0100

    feat: add basic Menu (dropdown) styling

--- src/_shared/theme/components/Menu.ts
+++ src/_shared/theme/components/Menu.ts
@@ -2,11 +2,22 @@
 import type { ComponentStyleConfig } from "@chakra-ui/react";
 
 export const Menu: ComponentStyleConfig = {
-  defaultProps: {
+  baseStyle: {
     list: {
-      _hover: {
-        bg: "brand.primary",
-      },
+      border: 'dark.s',
+      padding: '0',
+      borderRadius: 'sm'
     },
+    item: {
+      transition: 'all .5s ease-out',
+      borderBottom: 'dark.s',
+      ':last-of-type': {
+        borderBottom: 'none'
+      },
+      _hover: {
+        bg: 'brand.primary',
+        color: 'white.pure'
+      }
+    }
   },
 };
