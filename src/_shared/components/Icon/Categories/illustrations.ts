c8363191ad875eda78523bc5082db73fea79ebde
gabrieleAngius <gabrieleangius@protonmail.com>
2022-02-17 09:36:10 +0100

    feat: Add missing Status Icons
    closes #487

--- src/_shared/components/Icon/Categories/illustrations.ts
+++ src/_shared/components/Icon/Categories/illustrations.ts
@@ -1,9 +1,15 @@
 import { ReactComponent as Cookie } from '../SVG/Cookie.svg';
 import { ReactComponent as Frank } from '../SVG/Frank.svg';
 import { ReactComponent as Frank2 } from '../SVG/Frank2.svg';
+import { ReactComponent as Success } from '../SVG/Success.svg';
+import { ReactComponent as Error } from '../SVG/Error.svg';
+import { ReactComponent as Deleted } from '../SVG/Deleted.svg';
 
 export const illustrations = {
   Cookie,
   Frank,
   Frank2,
+  Success,
+  Error,
+  Deleted,
 };
