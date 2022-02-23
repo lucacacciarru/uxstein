92e5949889aff47f8776bb80a2e642cbaeb51813
gabrieleAngius <gabrieleangius@protonmail.com>
2022-02-23 10:52:38 +0100

    feat: add react-testing-library/hooks and test useExtendButton
    closes #498

--- src/_shared/components/SideNavigation/hooks/useExtendButton.test.tsx
+++ src/_shared/components/SideNavigation/hooks/useExtendButton.test.tsx
@@ -1,14 +1,44 @@
-// TODO: install testing-library
-import React from 'react';
-// import { render, screen } from '../../../../customRender';
-// import { useExtendButton } from './useExtendButton';
+import { useExtendButton } from './useExtendButton';
+import { renderHook } from '@testing-library/react-hooks';
+import { render, fireEvent, screen } from '../../../testConfig/customRender';
+import { Box, Button } from '@chakra-ui/react';
 
-describe('UseExtendButton hook', () => {
+describe('useExtendButton hook', () => {
   describe('isExtended', () => {
     test('should return false by default', () => {
-      //   const { isExtended } = renderHook(() => useExtendButton());
+      const { result } = renderHook(() => useExtendButton());
 
-      expect(false).toBeFalsy();
+      expect(result.current.isExtended).toBeFalsy();
+    });
+
+    test('shoud update isExtend after calling toggleIsExtended', () => {
+      const { result } = renderHook(() => useExtendButton());
+      const initialIsExtended = result.current.isExtended;
+
+      render(<Button {...result.current.iconButtonProps}> Click </Button>);
+      const button = screen.getByRole('button');
+      fireEvent.click(button);
+
+      expect(result.current.isExtended).toBe(!initialIsExtended);
+    });
+
+    test('shoud set isExtend = false when leave container', () => {
+      const { result } = renderHook(() => useExtendButton());
+
+      render(
+        <Box {...result.current.containerProps} data-testid="container">
+          <Button {...result.current.iconButtonProps}> Click </Button>
+        </Box>,
+      );
+
+      const button = screen.getByRole('button');
+      fireEvent.click(button);
+      expect(result.current.isExtended).toBeTruthy();
+
+      const container = screen.getByTestId('container');
+      fireEvent.mouseLeave(container);
+
+      expect(result.current.isExtended).toBeFalsy();
     });
   });
 });
