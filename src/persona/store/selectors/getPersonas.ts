5c3021e5858fb9c2d6850ea5f4e9a4f99379fc7c
Luca Cacciarru <luca.cacciarru@protonmail.com>
2022-03-07 12:52:29 +0100

    Fix: change getPersonas selector

--- src/persona/store/selectors/getPersonas.ts
+++ src/persona/store/selectors/getPersonas.ts
@@ -2,5 +2,5 @@ import { createSelector } from '@reduxjs/toolkit';
 import { baseSelector } from './baseSelectors';
 
 export const getPersonas = createSelector(baseSelector, personaState =>
-  Object.values(personaState.data.byId),
+  personaState.data.allIds.map(id => personaState.data.byId[id]),
 );
