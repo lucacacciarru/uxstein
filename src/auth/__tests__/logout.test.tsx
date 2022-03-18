300db723441b0bd68199e1a995424c07104f1ad5
Luca Cacciarru <luca.cacciarru@protonmail.com>
2022-03-18 12:57:18 +0100

    Fixed logout test

--- src/auth/__tests__/logout.test.tsx
+++ src/auth/__tests__/logout.test.tsx
@@ -1,33 +1,40 @@
 import { waitFor } from '@testing-library/react';
 import fetchMock from 'jest-fetch-mock';
 import { getStoreForTesting } from '../../_shared/testConfig/getStoreForTesting';
-import { User } from '../store';
-import { loginSuccess } from '../store/actions/login';
+import { AuthState } from '../store';
 import { logoutTrigger } from '../store/actions/logout';
 
-const users: User[] = [
-  { email: 'test@test.com', password: 'test', username: 'test' },
-];
+const initial_state: AuthState = {
+  profile: {
+    email: 'test@test.com',
+    password: 'test',
+    username: 'test',
+  },
+};
 
 describe('logout', () => {
   test('profile inside auth state should be undefined if loginTrigger succeeds', async () => {
-    fetchMock.doMock(() => Promise.resolve({ body: JSON.stringify(users) }));
-    const store = getStoreForTesting();
-    store.dispatch(loginSuccess(users[0]));
+    fetchMock.doMock(() => Promise.resolve({}));
+
+    const store = getStoreForTesting({ mocks: { auth: initial_state } });
     store.dispatch(logoutTrigger());
+
     await waitFor(() => {
       return store.getState().loading['auth'].loading === 0;
     });
+
     expect(store.getState().auth.profile).toBeUndefined();
   });
   test('profile inside auth state should be undefined if loginTrigger fails', async () => {
-    fetchMock.doMock(() => Promise.reject({ body: JSON.stringify(users) }));
-    const store = getStoreForTesting();
-    store.dispatch(loginSuccess(users[0]));
+    fetchMock.doMock(() => Promise.reject({}));
+
+    const store = getStoreForTesting({ mocks: { auth: initial_state } });
     store.dispatch(logoutTrigger());
+
     await waitFor(() => {
       return store.getState().loading['auth'].loading === 0;
     });
+
     expect(store.getState().auth.profile).toBeUndefined();
   });
 });
