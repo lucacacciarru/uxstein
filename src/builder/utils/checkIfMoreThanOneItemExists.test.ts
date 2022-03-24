b06246fd3abf3e3ea97252442601135063e3a78a
Luca Cacciarru <luca.cacciarru@protonmail.com>
2022-03-24 12:06:34 +0100

    Feat: Added checkIfMoreThanOneItemExists test

---
+++ src/builder/utils/checkIfMoreThanOneItemExists.test.ts
@@ -0,0 +1,31 @@
+import { ItemAttribute } from '../store/types';
+import { checkIfMoreThanOneItemExists } from './checkIfMoreThanOneItemExists';
+
+const MOCK_ATTRIBUTE_ITEMS_ONE_ELEMENT: ItemAttribute['items'] | undefined = [
+  { id: 'anyId', value: 'anyValue' },
+];
+const MOCK_ATTRIBUTE_ITEMS_MULTIPLE_ELEMENT:
+  | ItemAttribute['items']
+  | undefined = [
+  { id: 'anyId', value: 'anyValue' },
+  { id: 'anyOtherId', value: 'anyValue' },
+];
+
+describe('checkIfMoreThanOneItemExists function', () => {
+  test('if parameter is undefined, should return false', () => {
+    const returnValue = checkIfMoreThanOneItemExists(undefined);
+    expect(returnValue).toBeFalsy();
+  });
+  test('if parameter is contains only one item, should return true', () => {
+    const returnValue = checkIfMoreThanOneItemExists(
+      MOCK_ATTRIBUTE_ITEMS_ONE_ELEMENT,
+    );
+    expect(returnValue).toBeTruthy();
+  });
+  test('if parameter is contains multiple item, should return false', () => {
+    const returnValue = checkIfMoreThanOneItemExists(
+      MOCK_ATTRIBUTE_ITEMS_MULTIPLE_ELEMENT,
+    );
+    expect(returnValue).toBeFalsy();
+  });
+});
