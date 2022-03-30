f05921febb06d3a7f517fe40f2a3283101b765f1
Gabriele <gabriele@vlkstudio.com>
2022-03-30 07:52:24 +0000

    Apply suggestions from code review

--- src/_shared/utils/formatListToByIdObject.ts
+++ src/_shared/utils/formatListToByIdObject.ts
@@ -3,9 +3,9 @@ export function formatListToByIdObject<T extends { id: string }>(
   list: T[],
 ): Record<string, T> {
   return list.reduce(
-    (initial, persona) => ({
+    (initial, item) => ({
       ...initial,
-      [persona.id]: persona,
+      [item.id]: item,
     }),
     {},
   );
