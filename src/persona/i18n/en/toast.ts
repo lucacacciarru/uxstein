572cf524217ff4bea1cf69ee5db15675c79f3f96
nirreas <nicolas.porreca#gmail.com>
2022-04-07 14:48:24 +0200

    Added translations for SaveAsTemplateButton modal and toast

--- src/persona/i18n/en/toast.ts
+++ src/persona/i18n/en/toast.ts
@@ -1,4 +1,5 @@
 export const toast = {
   deletePersona: 'Persona has been successfully deleted',
   renamePersona: 'Persona has changed their name (not in the registry office)',
+  saveAsTemplate: 'Persona has been successfully saved as a Template'
 };
