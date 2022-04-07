baba3ebc6a53fc8798181f539fc8fa40835bb809
nirreas <nicolas.porreca#gmail.com>
2022-04-07 10:08:14 +0200

    Created New Persona Button component

---
+++ src/persona/components/NewPersonaFloatingActionButton/NewPersonaFloatingActionButton.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '../../../_shared/testConfig/customRender';
+import { NewPersonaFloatingActionButton } from './NewPersonaFloatingActionButton';
+import { PATHS } from '../../../_shared/types/paths';
+
+describe('NewPersonaFloatingActionButton component', () => {
+  test('should be rendered with the Create New Persona link', () => {
+    render(<NewPersonaFloatingActionButton />);
+
+    const newPersonaLink = screen.getByTestId('new-persona-link');
+
+    expect(newPersonaLink).toHaveAttribute(
+      'href',
+      `/${PATHS.PERSONAS}/${PATHS.CREATE_PERSONA}`,
+    );
+  });
+});
