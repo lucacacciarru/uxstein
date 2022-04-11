70ff71350e082c66ad71996a0753dc4e0d10b73a
nirreas <nicolas.porreca#gmail.com>
2022-04-11 17:57:25 +0200

    Fixed useBuilderEntity tests

--- src/builder/hooks/useBuilderEntity.test.ts
+++ src/builder/hooks/useBuilderEntity.test.ts
@@ -5,8 +5,8 @@ import { PATHS } from '../../_shared/types/paths';
 const MOCK_ENTITY_ID = '42';
 const MOCK_CREATE_PERSONA_ROUTE = `/${PATHS.PERSONAS}/create`;
 const MOCK_EDIT_PERSONA_ROUTE = `/${PATHS.PERSONAS}/${MOCK_ENTITY_ID}/edit`;
-const MOCK_CREATE_TEMPLATE_ROUTE = `/${PATHS.PERSONAS}/create`;
-const MOCK_EDIT_TEMPLATE_ROUTE = `/${PATHS.PERSONAS}/${MOCK_ENTITY_ID}/edit`;
+const MOCK_CREATE_TEMPLATE_ROUTE = `/${PATHS.TEMPLATES}/create`;
+const MOCK_EDIT_TEMPLATE_ROUTE = `/${PATHS.TEMPLATES}/${MOCK_ENTITY_ID}/edit`;
 
 describe('useBuilderEntity hook', () => {
   test('should retrieve the correct entityType from the current route', () => {
