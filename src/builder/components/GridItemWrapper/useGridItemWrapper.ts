8e334a1a402ce89535d93c796ead9ce162e415ab
gabrieleAngius <gabriele@vlkstudio.com>
2022-05-02 09:41:06 +0200

    Add useMemo inside useGridItemWrapper
    closes #616

--- src/builder/components/GridItemWrapper/useGridItemWrapper.ts
+++ src/builder/components/GridItemWrapper/useGridItemWrapper.ts
@@ -1,10 +1,16 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useSelectedBlock } from '../../hooks/useSelectedBlock';
 
 export const useGridItemWrapper = (id: string) => {
   const { selectedBlockId, selectBlock } = useSelectedBlock();
-  const isSelected = selectedBlockId !== id;
-  const opacity = selectedBlockId && isSelected ? '0.3' : '1';
+  const isSelected = useMemo(
+    () => selectedBlockId !== id,
+    [id, selectedBlockId],
+  );
+  const opacity = useMemo(
+    () => (selectedBlockId && isSelected ? '0.3' : '1'),
+    [isSelected, selectedBlockId],
+  );
 
   const select = useCallback(() => {
     selectBlock(id);
